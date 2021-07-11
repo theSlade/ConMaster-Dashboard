c59d9ad3d4e835e493ff4d868bf32e0dbbcdf377
LasseV12 <84160130+theSlade@users.noreply.github.com>
2021-07-11 23:18:52 +0200

    Nope

--- backend/src/routes/index.js
+++
@@ -1,8 +0,0 @@
-const router = require('express').Router();
-const discord = require('./discord')
-const auth = require('./auth')
-
-router.use("/auth", auth)
-router.use("/discord", discord)
-
-module.exports = router;
--- bot/src/database/database.js
+++
@@ -1,2 +0,0 @@
-const mongoose = require('mongoose');
-module.exports = mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
